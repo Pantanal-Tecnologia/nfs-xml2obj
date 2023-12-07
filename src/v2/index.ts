686b7d821d0b649040558ad10c3c34afc0c834b2
Jose <jose@iMac-de-Edezio.local>
2023-12-07 10:28:01 -0400

    fix: voltando tag de IMPOSTO pra ISS

--- src/v2/index.ts
+++ src/v2/index.ts
@@ -1,8 +1,7 @@
 import { V2Response } from 'data-nfs'
 
 import xml2js from 'xml2js'
-const LIB_VERSION = require('../version');
-
+const LIB_VERSION = require('../version')
 
 const getNumber = (item?: any) => {
   if (typeof item === 'undefined') return 0
@@ -165,39 +164,39 @@ const validators: Validators = {
   cpf: {
     text: [
       {
-        value: "cpf",
+        value: 'cpf',
         isLike: true,
       },
       {
-        value: "documento",
+        value: 'documento',
         isLike: true,
       },
     ],
     keyValidatorsFrom: [
       {
-        value: "emit",
+        value: 'emit',
         isLike: true,
         mandatory: true,
       },
       {
-        value: "prest",
+        value: 'prest',
         isLike: true,
         mandatory: true,
       },
     ],
     keyValidatorsTo: [
       {
-        value: "toma",
+        value: 'toma',
         isLike: true,
         mandatory: true,
       },
       {
-        value: "dest",
+        value: 'dest',
         isLike: true,
         mandatory: true,
       },
       {
-        value: "tom",
+        value: 'tom',
         isLike: true,
         mandatory: true,
       },
@@ -431,6 +430,10 @@ const validators: Validators = {
       },
       {
         value: 'valorImposto',
+        isLike: false,
+      },
+      {
+        value: 'IMPOSTO',
         isLike: false,
       },
     ],
@@ -842,7 +845,7 @@ function findVal(
 }
 
 const getDataNFSv2 = async (xmlString: string): Promise<V2Response> => {
-  console.log('running data-nf@' + LIB_VERSION);
+  console.log('running data-nf@' + LIB_VERSION)
   const stripPrefix = xml2js.processors.stripPrefix
   const result: any = await new Promise((resolve, reject) => {
     const parser = new xml2js.Parser({
@@ -884,11 +887,10 @@ const getDataNFSv2 = async (xmlString: string): Promise<V2Response> => {
       nfs,
       validators.cpf.text,
       validators.cpf.keyValidatorsFrom
-    );
+    )
 
-    const realCnpjDest = cnpjDest ? cnpjDest.replace(/\D/g, "") : undefined;
-    const realCpfEmit = cpfEmit ? cpfEmit.replace(/\D/g, "") : undefined;
-    
+    const realCnpjDest = cnpjDest ? cnpjDest.replace(/\D/g, '') : undefined
+    const realCpfEmit = cpfEmit ? cpfEmit.replace(/\D/g, '') : undefined
 
     const valorNF = findItemByList(
       nfs,
